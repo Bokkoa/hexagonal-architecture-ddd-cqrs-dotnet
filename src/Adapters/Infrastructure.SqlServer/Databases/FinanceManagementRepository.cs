f3dbb5e7af843963d43425d19ce48c56ab9507e0
Felipe Cruz <krivozznova@gmail.com>
2024-07-05 18:44:47 -0600

    feat(repo): adding repository impl

--- src/Adapters/Infrastructure.SqlServer/Databases/FinanceManagementRepository.cs
+++ src/Adapters/Infrastructure.SqlServer/Databases/FinanceManagementRepository.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Abstractions.Ports.Repositories;
+using Domain.Abstractions.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
-namespace Infrastructure.SqlServer.Databases
+namespace Infrastructure.SqlServer.Databases;
+
+public class FinanceManagementRepository : IFinanceManagementRepository
 {
-    internal class FinanceManagementRepository
+    private readonly DbContext _dbContext;
+
+    public FinanceManagementRepository(DbContext dbContext)
     {
+        _dbContext = dbContext;
+    }
+
+    public async Task InsertAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class
+    {
+        await _dbContext.Set<TEntity>().AddAsync(entity, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class
+    {
+        _dbContext.Set<TEntity>().Update(entity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<TEntity?> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken) where TEntity : class
+    {
+        return await _dbContext.Set<TEntity>().FirstOrDefaultAsync(expression, cancellationToken);
+    }
+
+    public async Task<List<TEntity>> ListAsync<TEntity>(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken) where TEntity : class
+    {
+        return await _dbContext.Set<TEntity>().Where(expression).ToListAsync(cancellationToken);
     }
 }
--- src/CoreBussiness/Application/Abstractions/Ports/Repositories/IFinanceManagementRepository.cs
+++ src/CoreBussiness/Application/Abstractions/Ports/Repositories/IFinanceManagementRepository.cs
@@ -1,9 +1,14 @@
-﻿namespace Application.Abstractions.Ports.Repositories;
+﻿using System.Linq.Expressions;
+
+namespace Application.Abstractions.Ports.Repositories;
 public interface IFinanceManagementRepository
 {
-    Task SaveAsync<TEntity>(TEntity entity,  CancellationToken cancellationToken)
+    Task InsertAsync<TEntity>(TEntity entity,  CancellationToken cancellationToken)
             where TEntity : class;
-    Task<TEntity> GetAsync<TEntity>(Guid id, CancellationToken cancellationToken);
-
-    IAsyncEnumerable<TEntity> GetAsync<TEntity>();
+    Task UpdateAsync<TEntity>(TEntity entity, CancellationToken cancellationToken)
+            where TEntity: class;
+    Task<TEntity?> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+            where TEntity: class;
+    Task<List<TEntity>> ListAsync<TEntity>(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+        where TEntity : class;
 }
