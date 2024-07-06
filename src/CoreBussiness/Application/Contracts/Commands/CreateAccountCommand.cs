f32ce9376710d3bf3e824763e7236e4408269189
Felipe Cruz <krivozznova@gmail.com>
2024-07-05 20:14:05 -0600

    feat(application): command handler account creation

--- src/CoreBussiness/Application/Contracts/Commands/CreateAccountCommand.cs
+++ src/CoreBussiness/Application/Contracts/Commands/CreateAccountCommand.cs
@@ -1,6 +1,6 @@
 ﻿using Application.Abstractions.Contracts;
 
 namespace Application.Contracts.Commands;
-public record CreateAccountCommand(string FirstName, string LastName, string email) : Message, ICommand
+public record CreateAccountCommand(string FirstName, string LastName, string Email) : Message, ICommand
 {
 }
